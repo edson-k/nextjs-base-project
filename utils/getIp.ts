5c196c5751e49d305d3cb7fd9e208cab29c8cfcf
Édson Kuzniewski <contato@edsonk.com.br>
2024-05-26 17:10:20 -0300

    logs for ajust getip

--- utils/getIp.ts
+++ utils/getIp.ts
@@ -5,11 +5,11 @@ let ip: string = '';
 
 getServerSideProps: async (context: GetServerSidePropsContext) => {
     ip = context.req.socket.remoteAddress || '';
-    console.log('ip 01', ip);
 }
 
 export const GetIp = () => {
     const header = headers()
+    console.log('ip 01', ip);
     console.log('ip 02', header.get('cf-connecting-ip'));
     console.log('ip 03', header.get('x-forwarded-for'));
     const remoteIp = header.get('cf-connecting-ip') ?
