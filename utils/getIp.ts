6e9d65b99ada8f515aafe5ddc18ccf7e3aeed0c8
Édson Kuzniewski <contato@edsonk.com.br>
2024-05-25 22:28:04 -0300

    ajust get ip

--- utils/getIp.ts
+++ utils/getIp.ts
@@ -1,7 +1,20 @@
+import { GetServerSidePropsContext } from 'next'
 import { headers } from 'next/headers'
+
+let ip: any = '';
+
+getServerSideProps: async (context: GetServerSidePropsContext) => {
+    ip = context.req.socket.remoteAddress;
+}
 
 export const GetIp = () => {
     const header = headers()
-    const ip = header.get('cf-connecting-ip') ? (header.get('cf-connecting-ip') ?? '127.0.0.1').split(',')[0] : (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
-    return ip;
+    const remoteIp = header.get('cf-connecting-ip') ?
+        (header.get('cf-connecting-ip') ?? '127.0.0.1').split(',')[0]
+        :
+        header.get('x-forwarded-for') ?
+            (header.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
+            :
+            ip;
+    return remoteIp;
 }
